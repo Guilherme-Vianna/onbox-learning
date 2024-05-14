aa6e80a7720019959f7a77d22f38c7e84072d630
Guilherme Vianna <mataveli91@gmail.com>
2024-05-14 13:20:03 -0700

    initial commit

---
+++ OnBoxTeste/Revit/App.cs
@@ -0,0 +1,56 @@
+﻿using Autodesk.Revit.UI;
+using Onbox.Abstractions.V9;
+using Onbox.Core.V9;
+using Onbox.Mvc.Revit.V9;
+using Onbox.Mvc.V9.Messaging;
+using Onbox.Revit.V9;
+using Onbox.Revit.V9.Applications;
+using Onbox.Revit.V9.UI;
+using OnBoxTeste.Revit.Commands;
+using OnBoxTeste.Revit.Commands.Availability;
+using OnBoxTeste.Views;
+
+namespace OnBoxTeste.Revit
+{
+    [ContainerProvider("1e515b08-3b46-4235-84d2-45301b22d26f")]
+    public class App : RevitApp
+    {
+        public override void OnCreateRibbon(IRibbonManager ribbonManager)
+        {
+            // Here you can create Ribbon tabs, panels and buttons
+
+            var br = ribbonManager.GetLineBreak();
+
+            // Adds a Ribbon Panel to the Addins tab
+            var addinPanelManager = ribbonManager.CreatePanel("OnBoxTeste");
+            addinPanelManager.AddPushButton<HelloCommand, AvailableOnProject>($"Hello{br}Framework", "onbox_logo");
+
+            // Adds a new Ribbon Tab with a new Panel
+            var panelManager = ribbonManager.CreatePanel("OnBoxTeste", "Hello Panel");
+            panelManager.AddPushButton<HelloCommand, AvailableOnProject>($"Hello{br}Framework", "onbox_logo");
+            panelManager.AddPushButton<WPFViewCommand, AvailableOnProject>($"Hello{br}WPF", "autodesk_logo");
+        }
+
+        public override Result OnStartup(IContainer container, UIControlledApplication application)
+        {
+            // Dependencias para criar o container *OBRIGATORIO
+            container.AddOnboxCore();
+            container.AddRevitMvc();
+
+            // Registers IWPFView to the container
+            // Views should ALWAYS be added as Transients
+            container.AddTransient<IHelloWpfView, HelloWpfView>();
+
+            // ADICAO DOS SERVICOS
+            container.AddSingleton<IMessageService, MessageBoxService>();
+
+            return Result.Succeeded;
+        }
+
+        public override Result OnShutdown(IContainerResolver container, UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+
+}
---
+++ OnBoxTeste/Revit/Commands/WPFViewCommand.cs
@@ -0,0 +1,22 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Onbox.Abstractions.V9;
+using Onbox.Revit.V9.Commands;
+using OnBoxTeste.Views;
+
+namespace OnBoxTeste.Revit.Commands
+{
+    [Transaction(TransactionMode.Manual)]
+    public class WPFViewCommand : RevitAppCommand<App>
+    {
+        public override Result Execute(IContainerResolver container, ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // Asks the container for a new instance of a view
+            var wpfView = container.Resolve<IHelloWpfView>();
+            wpfView.ShowDialog();
+
+            return Result.Succeeded;
+        }
+    }
+}
